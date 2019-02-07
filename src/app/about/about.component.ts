1ba584728331f1b751eb217b9c141624e96617db
nhatmai-dev <30576796+nhatmai-dev@users.noreply.github.com>
2019-02-07 12:04:40 +0700

    implements the "About" component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,6 +10,7 @@ import { ElectronService } from 'ngx-electron';
 export class AboutComponent implements ContentComponent {
 
   @Input() data: any;
+  appVersion: string;
   angularVersion: string;
   nodeVersion: string;
   electronVersion: string;
@@ -18,26 +19,7 @@ export class AboutComponent implements ContentComponent {
 
   constructor(private _electronService: ElectronService, private _ngZone: NgZone) {
     this.angularVersion = VERSION.full;
-    this._electronService.ipcRenderer.on('get-node-version-resp', (event, arg) => {
-      this._ngZone.run(() => {
-        this.nodeVersion = arg;
-      });
-    });
-    this._electronService.ipcRenderer.on('get-electron-version-resp', (event, arg) => {
-      this._ngZone.run(() => {
-        this.electronVersion = arg;
-      });
-    });
-    this._electronService.ipcRenderer.on('get-chrome-version-resp', (event, arg) => {
-      this._ngZone.run(() => {
-        this.chromeVersion = arg;
-      });
-    });
-    this._electronService.ipcRenderer.on('get-v8-version-resp', (event, arg) => {
-      this._ngZone.run(() => {
-        this.v8Version = arg;
-      });
-    });
+    this.getAppVersion();
     this.getNodeVersion();
     this.getElectronVersion();
     this.getChromeVersion();
@@ -45,19 +27,47 @@ export class AboutComponent implements ContentComponent {
   }
 
   getNodeVersion() {
+    this._electronService.ipcRenderer.on('get-node-version-resp', (event, arg) => {
+      this._ngZone.run(() => {
+        this.nodeVersion = arg;
+      });
+    });
     this._electronService.ipcRenderer.send('get-node-version');
   }
 
   getElectronVersion() {
+    this._electronService.ipcRenderer.on('get-electron-version-resp', (event, arg) => {
+      this._ngZone.run(() => {
+        this.electronVersion = arg;
+      });
+    });
     this._electronService.ipcRenderer.send('get-electron-version');
   }
 
   getChromeVersion() {
+    this._electronService.ipcRenderer.on('get-chrome-version-resp', (event, arg) => {
+      this._ngZone.run(() => {
+        this.chromeVersion = arg;
+      });
+    });
     this._electronService.ipcRenderer.send('get-chrome-version');
   }
 
   getV8Version() {
+    this._electronService.ipcRenderer.on('get-v8-version-resp', (event, arg) => {
+      this._ngZone.run(() => {
+        this.v8Version = arg;
+      });
+    });
     this._electronService.ipcRenderer.send('get-v8-version');
+  }
+  getAppVersion() {
+    this._electronService.ipcRenderer.on('get-app-version-resp', (event, arg) => {
+      this._ngZone.run(() => {
+        this.appVersion = arg;
+      });
+    });
+    this._electronService.ipcRenderer.send('get-app-version');
   }
 
   getCodeName(): string {
